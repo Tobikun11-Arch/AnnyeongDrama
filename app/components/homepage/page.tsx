9542c451d0d37cedc76f0c428334ef6287526a8f
Joenel <joenelsevellejo831@gmail.com>
2025-03-11 17:18:19 +0800

    Fetch: Airing kdrama, axios and tanstack query applied

--- app/components/homepage/page.tsx
+++ app/components/homepage/page.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TopRatingBanner from './TopRatingBanner'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const fetchdata = async() => {
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_genres=18&with_origin_country=KR`, {
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${API_KEY}`,
+            }
+        })
+        return response.data.results
+    } catch (error) {
+        console.error('Error fetching genres:', error);
+    }
+}
 
 export default function Page() {
+    const { data } = useQuery({
+        queryKey: ['Kdrama'],
+        queryFn: fetchdata
+    })
+
+    console.log("data: ", data)
+
     return (
         <div>
             <TopRatingBanner/>
+            <button className='bg-black text-white flex justify-center items-center' onClick={fetchdata}>Test data</button>
         </div>
     )
 }
--- app/components/user-profile/Recommendations.tsx
+++ app/components/user-profile/Recommendations.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Recommendations() {
+    
     return (
         <div className='w-60 h-96 bg-black'>
             
