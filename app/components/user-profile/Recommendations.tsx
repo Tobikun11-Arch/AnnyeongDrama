fc300a6cc40f913245b83a40a69fba54ef366c71
Joenel <joenelsevellejo831@gmail.com>
2025-03-11 14:13:42 +0800

    [Responsive]: My account page responsiveness✅

---
+++ app/components/user-profile/Recommendations.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Recommendations() {
+    return (
+        <div className='w-60 h-96 bg-black'>
+            
+        </div>
+    )
+}
--- app/pages/UserProfile.tsx
+++ app/pages/UserProfile.tsx
@@ -4,7 +4,7 @@ import { useUserData } from '../state/UserData'
 import Image from 'next/image'
 import { PenLine, Play } from 'lucide-react';
 import EditProfile from '../components/user-profile/EditProfile';
-
+import Recommendations from '../components/user-profile/Recommendations';
 export default function UserProfile() {
     const { userdata } = useUserData()
     let [isOpen, setIsOpen] = useState(false)
@@ -18,11 +18,12 @@ export default function UserProfile() {
     }
 
     return (
-        <main className='p-6'>
+        <main className='p-3 sm:p-4'>
             {userdata.map((user, index)=> (
                 <div key={`${user.Username}-${index}`} >
-                    <section className='w-full flex justify-between px-32 pb-4'>
-                        <div className='flex gap-2 justify-center'>
+                    <section className='w-full flex flex-col sm:flex-row justify-between lg:px-32 pb-4 text-sm'>
+
+                        <div className='flex gap-2 sm:justify-center'>
                             <div className='relative h-24 w-24 overflow-hidden rounded-full'>
                                 <Image
                                     fill
@@ -33,7 +34,7 @@ export default function UserProfile() {
                                 />
                             </div>
                             <div>
-                                <h1 className='font-semibold text-xl'>{user.Username}</h1>
+                                <h1 className='font-semibold sm:text-xl'>{user.Username}</h1>
                                 <div className='flex items-center'>
                                     <Play size={16} className='mt-0.5'/>
                                     <h4 className='font-semibold text-red-500'>{user.Watching}</h4>
@@ -41,30 +42,29 @@ export default function UserProfile() {
                             </div>
                         </div>
 
-                        <div className='flex flex-col justify-between items-end'>
-                            <div className='flex gap-4 font-semibold text-lg'>
+                        <div className='flex flex-col gap-2 sm:gap-0 justify-between items-end'>
+                            <div className='flex gap-4 font-semibold sm:text-lg'>
                                 <h1>{user.Following} following</h1> 
                                 <h1>{user.Follower} follower</h1>
                             </div>
-                            <button className='border border-gray-300 py-2 w-32 hover:bg-blue-600 hover:text-white flex gap-2 items-center text-sm justify-center rounded-lg' onClick={openModal}>
+                            <button className='border border-gray-300 py-1 sm:py-2 w-24 sm:w-32 hover:bg-blue-600 hover:text-white flex gap-2 items-center text-sm justify-center rounded-lg' onClick={openModal}>
                                 <PenLine size={15} className='mt-0.5'/>
                                 Edit
                             </button>
                         </div>
                         <EditProfile isOpen={isOpen} closeModal={closeModal}/>
+
                     </section>
                     <hr />
                     <div className='flex flex-col justify-center items-center mt-5 py-2'>
                         <h1 className='font-italic font-semibold text-2xl mb-1'>About me</h1>
                         <div className='flex items-center gap-1'>
-                            <h1 className='text-gray-500 text-sm px-36 text-center'>{user.AboutMe ? user.AboutMe : 'Add a short bio.'}</h1>
+                            <h1 className='text-gray-500 text-sm md:px-36 lg:px-52 text-center'>{user.AboutMe ? user.AboutMe : 'Add a short bio.'}</h1>
                         </div>
                     </div>
 
                     <h1 className='mt-8 text-2xl font-bold font-mono'>Recommmendations</h1>
-                    <div>
-
-                    </div>
+                    <Recommendations/>
                 </div>
             ))}
         </main>
