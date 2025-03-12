dd43a688b4944c02ee9def21b73c47873541a277
Joenel <joenelsevellejo831@gmail.com>
2025-03-12 11:52:32 +0800

    Image: Image pagination added

--- app/api/KdramaApi.ts
+++ app/api/KdramaApi.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+export let totalPages = 0
+
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY
-export const fetchdata = async() => {
+export const fetchdata = async(page: number) => {
     try {
         const response = await axios.get(`https://api.themoviedb.org/3/discover/tv`, {
             headers: {
@@ -12,14 +14,21 @@ export const fetchdata = async() => {
                 api_key: API_KEY,
                 language: 'en-US', 
                 with_origin_country: 'KR',
-                sort_by: 'first_air_date.desc', 
                 with_status: 'Returning Series',
+                "first_air_date.gte": "2025-01-01",
+                "first_air_date.lte": "2025-12-31",
+                vote_count_gte: 10, 
+                page: page
             },
         });
-
-        console.log("response: ", response.data.page)
+        const filteredResults = response.data.results.filter(
+            (drama: { poster_path: any; }) => drama.poster_path
+        );
+        
+        const limitedResults = filteredResults.slice(0, 12);
+        totalPages = response.data.total_pages
           
-        return response.data.results
+        return limitedResults
     } catch (error) {
         console.error('Error fetching genres:', error);
         return []
--- app/components/homepage/page.tsx
+++ app/components/homepage/page.tsx
@@ -1,23 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import TopRatingBanner from './TopRatingBanner'
 import { fetchdata } from '@/app/api/KdramaApi'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { TMDBResponse } from '@/app/types/dramaData'
 import KdramaList from './KdramaList'
-
-
+import { totalPages } from '@/app/api/KdramaApi'
 
 export default function Page() {
+    const [currentPage, setCurrentPage] = useState(1);
+
     const { isLoading, data } = useSuspenseQuery<TMDBResponse>({
-        queryKey: ['Kdrama'],
-        queryFn: fetchdata
+        queryKey: ['Kdrama', currentPage],
+        queryFn: ()=> fetchdata(currentPage)
     })
 
     if(isLoading) {
         return <h1>Loading</h1>
     }
-    
-    console.log("Data: ", data)
+
+    function paginationImage(action: string) {
+        if (action === "previous" && currentPage > 1) {
+            setCurrentPage(prev => prev - 1);
+            return
+        } else if(action === "next" && currentPage <  totalPages) {
+            setCurrentPage(prev => prev + 1);
+        }
+
+        return currentPage
+
+    }
 
     return (
         <main>
@@ -25,6 +37,12 @@ export default function Page() {
             
             <h1>Now Airing K-Dramas</h1>
             <KdramaList drama={data} />
+
+            <div>
+                <button onClick={()=> paginationImage('previous')}>Previous</button>
+                <p>{currentPage}</p>
+                <button onClick={()=> paginationImage('next')}>Next</button>
+            </div>
 
         </main>
     )
